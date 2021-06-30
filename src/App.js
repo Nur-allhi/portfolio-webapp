d9bfcf9b3202a9f68aed62bbedaa31080264eef2
Nur-allhi <noorefty1@gmail.com>
2021-06-30 11:51:50 +0600

    v-0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import NavbarMain from "./Components/Navbar/NavbarMain";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <NavbarMain />
-    </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar/NavbarMain.js
+++ src/Components/Navbar/NavbarMain.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 
 const NavbarMain = () => {
   return (
-    <div>
-      <h1>its navbar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Nur</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
   );
 };
 
